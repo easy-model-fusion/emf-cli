a12207761318beb42a2032d13dc1ba07c3e9de22
Zapharaos <matthieu.freitag@gmail.com>
2024-01-25 12:49:05 +0100

    Root command

--- internal/command/root.go
+++ internal/command/root.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"github.com/easy-model-fusion/client/internal/app"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -10,6 +12,27 @@ var rootCmd = &cobra.Command{
 	Use:   "emf-client",
 	Short: "EMF client is a command line tool to manage a EMF project easily",
 	Long:  `EMF client is a command line tool to manage a EMF project easily.`,
+	Run:   runBase,
+}
+
+func runBase(cmd *cobra.Command, args []string) {
+
+	// get all commands
+	var commandList []string
+	for _, child := range cmd.Commands() {
+		commandList = append(commandList, child.Use)
+	}
+
+	// allow the user to choose one command
+	selectedCommand, _ := pterm.DefaultInteractiveSelect.WithOptions(commandList).Show()
+
+	// run the selected command
+	selectedChild, _, _ := cmd.Find([]string{selectedCommand})
+	if selectedChild != nil {
+		selectedChild.Run(cmd, args)
+	} else {
+		app.L().WithTime(false).Error("Selected command " + selectedCommand + " is not recognized")
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
