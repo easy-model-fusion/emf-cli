68a3b15868c078e6bd9a51d30bbc6929789408ba
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-01-25 16:04:30 +0100

    #4 remove useless comments

--- internal/confgen/models.go
+++ internal/confgen/models.go
@@ -4,14 +4,9 @@ import (
 	"github.com/spf13/viper"
 )
 
-func AddModel(models []string) error { // Get the current working directory
-	// Access and print the original models list
+func AddModel(models []string) error {
 	originalModelsList := viper.GetStringSlice("models")
-
-	// Add additional items to the models list
 	updatedModelsList := append(originalModelsList, models...)
-
-	// Update the models list in the configuration
 	viper.Set("models", updatedModelsList)
 
 	// Attempt to write the configuration file
