45bdffd670bc85730167f37a480d98e4f6e3c5b4
vhahnschutz <victorhahnschutz@gmail.com>
2024-02-16 09:52:36 +0100

    add clean temp file and build file

---
+++ internal/command/clean.go
@@ -0,0 +1,66 @@
+package command
+
+import (
+	"github.com/easy-model-fusion/client/internal/config"
+	"github.com/easy-model-fusion/client/internal/sdk"
+	"github.com/easy-model-fusion/client/internal/model"
+	"github.com/easy-model-fusion/client/internal/utils"
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+// cleanCmd represents the clean command
+var cleanCmd = &cobra.Command{
+	Use:   	"clean",
+	Short: 	"Clean project",
+	Long: 	"Clean project",
+	Run:   	runClean,
+}
+
+func runClean(cmd *cobra.Command, args []string) {
+	// extensions file removed
+    extensions := []string{".exe", ".o", ".obj", ".out"}
+
+    // Get the current working directory
+    currentDir, err := os.Getwd()
+    if err != nil {
+        return err
+    }
+
+   	// Walk through all files in the current working directory
+    err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            pterm.Error.Printfln("Operation failed.")
+			return
+        }
+
+        // Check if the file has one of the specified extensions to be removed
+        for _, ext := range extensions {
+            if filepath.Ext(path) == ext {
+                // Remove the file
+                err := os.Remove(path)
+                if err != nil {
+                    pterm.Error.Printfln("Operation failed.")
+					return
+                }
+				pterm.Success.Printfln("Operation succeeded, file %s\n deleted", pat)
+                break
+            }
+        }
+
+        return nil
+    })
+
+    if err == nil {
+		pterm.Success.Printfln("Operation succeeded.")
+	} else {
+		pterm.Error.Printfln("Operation failed.")
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(cleanCmd)
+}
+
