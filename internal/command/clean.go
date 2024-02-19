2fe75c9f2403a9edc314af3b4af22daa63dd355b
vhahnschutz <vhahnschutz@gmail.com>
2024-02-19 16:49:49 +0100

    clean commande with build dir

--- internal/command/clean.go
+++ internal/command/clean.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"github.com/easy-model-fusion/client/internal/config"
+	"github.com/easy-model-fusion/client/internal/sdk"
 	"github.com/easy-model-fusion/client/internal/utils"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
@@ -9,8 +10,8 @@ import (
 	"path/filepath"
 )
 
-var allFlag bool
-var authorizeAll bool
+var allFlagDelete bool
+var authorizeAllDelete bool
 
 // cleanCmd represents the clean command
 var cleanCmd = &cobra.Command{
@@ -21,68 +22,74 @@ var cleanCmd = &cobra.Command{
 }
 
 func runClean(cmd *cobra.Command, args []string)  {
-	
-	if allFlag {
-		if !authorizeAll{
+	if allFlagDelete {
+		if !authorizeAllDelete{
 			yes := utils.AskForUsersConfirmation("Are you sure you want to delete all downloaded models and clean the build files of this project?")
 			if !yes {
 				return
 			}
 		}
+		if config.GetViperConfig() != nil {
+			return
+		}
+		sdk.SendUpdateSuggestion()
 		err := config.RemoveAllModels()
 		if err == nil {
 			pterm.Success.Printfln("Operation succeeded.")
 		}
 	}
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	currentDir, errr := os.Getwd()
+	if errr != nil {
+		pterm.Error.Printfln("Operation failed.")
+		return
+	}
+
+	err := filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			pterm.Error.Printfln("Operation failed.")
 			return err
 		}
-		if err := deleteDir(path); err != nil {
-            pterm.Error.Printfln("Failed to delete directory: %v", err)
-            return err
-        }
+		if info.IsDir() && info.Name() == "build" {
+			err := deleteDir(path)
+			if err != nil {
+				return err
+			}
+		}
 		return nil
 	})
+	if err == nil {
+		pterm.Success.Printfln("Operation succeeded.")
+	} else {
+		pterm.Error.Printfln("Operation failed.", err)
+	}
 	return
 }
 
 func deleteDir(dossier string) error {
 	dir, err := os.Open(dossier)
 	if err != nil {
-		pterm.Error.Printfln("Operation failed.")
-		return
+		return err
 	}
 	defer dir.Close()
 
 	files, err := dir.Readdir(0)
 	if err != nil {
-		pterm.Error.Printfln("Operation failed.")
-		return
+		return err
 	}
 
 	for _, file := range files {
 		fileName := file.Name()
-		filePath := filepath.Join(filePath, fileName)
-
-		err = os.RemoveAll(filePath)
-		if err != nil {
-			pterm.Error.Printfln("Operation failed.")
-			return
-		}
-	}
-	if err == nil {
-		pterm.Success.Printfln("Operation succeeded.")
-	} else {
-		pterm.Error.Printfln("Operation failed.")
-	}
+		fileToRemove := filepath.Join(dossier, fileName)
+		if err := os.Remove(fileToRemove); err != nil {
+            return err
+        }
+    }
+    return nil
 }
 
 func init() {
-	cleanCmd.Flags().BoolVarP(&allFlag, "all", "a", false, "clean all project")
-	cleanCmd.Flags().BoolVarP(&authorizeAll, "yes", "y", false, "authorize all deletions")
+	cleanCmd.Flags().BoolVarP(&allFlagDelete, "all", "a", false, "clean all project")
+	cleanCmd.Flags().BoolVarP(&authorizeAllDelete, "yes", "y", false, "authorize all deletions")
 	rootCmd.AddCommand(cleanCmd)
 }
 
