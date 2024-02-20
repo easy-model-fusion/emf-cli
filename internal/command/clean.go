645e36c655c1a83c0cdd3348a03311aae328ff4a
vhahnschutz <victorhahnschutz@gmail.com>
2024-02-20 14:27:17 +0100

    modif function and remove

--- internal/command/clean.go
+++ internal/command/clean.go
@@ -12,19 +12,21 @@ import (
 var allFlagDelete bool
 var authorizeAllDelete bool
 
+const dirName = "build"
+
 // cleanCmd represents the clean command
 var cleanCmd = &cobra.Command{
-	Use:   	"clean",
-	Short: 	"Clean project",
-	Long: 	"Clean project",
-	Run:   	runClean,
+	Use:   "clean",
+	Short: "Clean project",
+	Long:  "Clean project",
+	Run:   runClean,
 }
 
-func runClean(cmd *cobra.Command, args []string)  {
+func runClean(cmd *cobra.Command, args []string) {
 	// Delete all models if flag --all
 	if allFlagDelete {
-		// Ask for confirmation 
-		if !authorizeAllDelete{
+		// Ask for confirmation
+		if !authorizeAllDelete {
 			yes := utils.AskForUsersConfirmation("Are you sure you want to delete all downloaded models and clean the build files of this project?")
 			if !yes {
 				return
@@ -36,10 +38,10 @@ func runClean(cmd *cobra.Command, args []string)  {
 		err := config.RemoveAllModels()
 		if err == nil {
 			pterm.Success.Printfln("Operation succeeded.")
-		} else{
+		} else {
 			pterm.Error.Printfln("Operation failed.")
 		}
-		
+
 	}
 
 	// Get the current dir
@@ -48,52 +50,19 @@ func runClean(cmd *cobra.Command, args []string)  {
 		pterm.Error.Printfln("Operation failed.")
 		return
 	}
+	buildDir := filepath.Join(currentDir, dirName)
 
-	// Search build dir in the current dir
-	err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		// delete all file and dir in build
-		if info.IsDir() && info.Name() == "build" {
-			err := deleteDir(path)
-			if err != nil {
-				return err
-			}
-			// skip search in build and continue
-			return filepath.SkipDir
-		}
-		return nil
-	})
+	_, err = os.Stat(buildDir)
+	if os.IsNotExist(err) {
+		pterm.Success.Printfln("Operation succeeded.")
+		return
+	}
+	err = os.RemoveAll(buildDir)
 	if err == nil {
 		pterm.Success.Printfln("Operation succeeded.")
 	} else {
 		pterm.Error.Printfln("Operation failed.")
 	}
-}
-
-func deleteDir(dossier string) error {
-	// Open the dir
-	dir, err := os.Open(dossier)
-	if err != nil {
-		return err
-	}
-	// Close the dir at the end of function
-	defer dir.Close()
-
-	files, err := dir.Readdir(0)
-	if err != nil {
-		return err
-	}
-	// Delete files and dirs one by one
-	for _, file := range files {
-		fileName := file.Name()
-		fileToRemove := filepath.Join(dossier, fileName)
-		if err := os.Remove(fileToRemove); err != nil {
-            return err
-        }
-    }
-    return nil
 }
 
 func init() {
