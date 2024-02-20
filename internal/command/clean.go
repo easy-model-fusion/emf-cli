3d12e7e4ac627eeb08406e5ed11230588c74264b
vhahnschutz <vhahnschutz@gmail.com>
2024-02-20 08:58:32 +0100

    clean command functional

--- internal/command/clean.go
+++ internal/command/clean.go
@@ -22,7 +22,9 @@ var cleanCmd = &cobra.Command{
 }
 
 func runClean(cmd *cobra.Command, args []string)  {
+	// Delete all models if flag --all
 	if allFlagDelete {
+		// Ask for confirmation 
 		if !authorizeAllDelete{
 			yes := utils.AskForUsersConfirmation("Are you sure you want to delete all downloaded models and clean the build files of this project?")
 			if !yes {
@@ -36,47 +38,57 @@ func runClean(cmd *cobra.Command, args []string)  {
 		err := config.RemoveAllModels()
 		if err == nil {
 			pterm.Success.Printfln("Operation succeeded.")
+		} else{
+			pterm.Error.Printfln("Operation failed.")
 		}
+		
 	}
 
-	currentDir, errr := os.Getwd()
-	if errr != nil {
+	// Get the current dir
+	currentDir, err := os.Getwd()
+	if err != nil {
 		pterm.Error.Printfln("Operation failed.")
 		return
 	}
 
-	err := filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
+	// Search build dir in the current dir
+	err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+		// delete all file and dir in build
 		if info.IsDir() && info.Name() == "build" {
 			err := deleteDir(path)
 			if err != nil {
 				return err
 			}
+			// skip search in build and continue
+			return filepath.SkipDir
 		}
 		return nil
 	})
 	if err == nil {
 		pterm.Success.Printfln("Operation succeeded.")
 	} else {
-		pterm.Error.Printfln("Operation failed.", err)
+		pterm.Error.Printfln("Operation failed.")
 	}
 	return
 }
 
 func deleteDir(dossier string) error {
+	// Open the dir
 	dir, err := os.Open(dossier)
 	if err != nil {
 		return err
 	}
+	// Close the dir at the end of function
 	defer dir.Close()
 
 	files, err := dir.Readdir(0)
 	if err != nil {
 		return err
 	}
-
+	// Delete files and dirs one by one
 	for _, file := range files {
 		fileName := file.Name()
 		fileToRemove := filepath.Join(dossier, fileName)
