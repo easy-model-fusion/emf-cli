8cf4295d1ef148250c953c51d007ee914fba3a80
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-02-21 23:07:02 +0100

    #41  fixed unit tests

--- internal/huggingface/list_test.go
+++ internal/huggingface/list_test.go
@@ -2,20 +2,20 @@ package huggingface
 
 import (
 	"github.com/easy-model-fusion/emf-cli/internal/model"
+	"github.com/easy-model-fusion/emf-cli/internal/utils"
 	"github.com/easy-model-fusion/emf-cli/test"
 	"reflect"
 	"testing"
 )
 
 func TestGetModels(t *testing.T) {
-	limit := 10
 	h := NewHuggingFace(BaseUrl, "")
 	models, err := h.GetModels(model.TEXT_TO_IMAGE, 10)
 	test.AssertEqual(t, err, nil, "The api call should've passed.")
-	test.AssertEqual(t, len(models), limit, "The api call should've returned 10 models.")
 
 	for _, apiModel := range models {
-		test.AssertNotEqual(t, apiModel.Name, "", "Model's name should not be empty.")
+		valid := utils.SliceContainsItem(model.AllModules, apiModel.Config.Module)
+		test.AssertEqual(t, valid, true, "Model's module should be valid.")
 	}
 }
 
