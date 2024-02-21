2f85fcc196d9d03d602ece5b1789ee2de1d3be86
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-02-21 22:49:24 +0100

    #41 added modules filter

--- internal/huggingface/list_test.go
+++ internal/huggingface/list_test.go
@@ -3,6 +3,7 @@ package huggingface
 import (
 	"github.com/easy-model-fusion/emf-cli/internal/model"
 	"github.com/easy-model-fusion/emf-cli/test"
+	"reflect"
 	"testing"
 )
 
@@ -15,5 +16,72 @@ func TestGetModels(t *testing.T) {
 
 	for _, apiModel := range models {
 		test.AssertNotEqual(t, apiModel.Name, "", "Model's name should not be empty.")
+	}
+}
+
+func Test_getModelsByModules(t *testing.T) {
+	validModels := []model.Model{
+		{
+			Name: "model1",
+			Config: model.Config{
+				Module: model.DIFFUSERS,
+			},
+		},
+		{
+			Name: "model2",
+			Config: model.Config{
+				Module: model.TRANSFORMERS,
+			},
+		},
+		{
+			Name: "model3",
+			Config: model.Config{
+				Module: model.DIFFUSERS,
+			},
+		},
+	}
+	invalidModels := []model.Model{
+		{
+			Name: "model1",
+			Config: model.Config{
+				Module: model.DIFFUSERS,
+			},
+		},
+		{
+			Name: "model2",
+			Config: model.Config{
+				Module: model.TRANSFORMERS,
+			},
+		},
+		{
+			Name: "model3",
+			Config: model.Config{
+				Module: model.DIFFUSERS,
+			},
+		},
+		{
+			Name: "invalid_model",
+			Config: model.Config{
+				Module: "INVALID",
+			},
+		},
+	}
+	type args struct {
+		models []model.Model
+	}
+	tests := []struct {
+		name               string
+		args               args
+		wantReturnedModels []model.Model
+	}{
+		{name: "All valid models", args: args{validModels}, wantReturnedModels: validModels},
+		{name: "Not all valid models", args: args{invalidModels}, wantReturnedModels: validModels},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotReturnedModels := getModelsByModules(tt.args.models); !reflect.DeepEqual(gotReturnedModels, tt.wantReturnedModels) {
+				t.Errorf("getModelsByModules() = %v, want %v", gotReturnedModels, tt.wantReturnedModels)
+			}
+		})
 	}
 }
