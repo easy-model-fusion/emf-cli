e1a1fbd4e2c7364b10a5f5996514fc9592f81a44
SchawnnDev <iperskill@gmail.com>
2024-02-22 02:22:22 +0100

    continue build cmd

--- internal/command/build.go
+++ internal/command/build.go
@@ -1,16 +1,21 @@
 package command
 
 import (
-	"github.com/easy-model-fusion/client/internal/config"
-	"github.com/easy-model-fusion/client/internal/sdk"
+	"github.com/easy-model-fusion/emf-cli/internal/config"
+	"github.com/easy-model-fusion/emf-cli/internal/sdk"
+	"github.com/easy-model-fusion/emf-cli/internal/utils"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"os"
+	"os/exec"
 )
 
 var (
-	buildDestination = "/dist"
-	buildCustomName  string
+	buildDestination   = "/dist"
+	buildCustomName    string
+	buildOneFile       bool
+	buildCompress      bool
+	buildIncludeModels bool
 )
 
 // buildCmd represents the build command
@@ -22,7 +27,7 @@ var buildCmd = &cobra.Command{
 }
 
 func runBuild(cmd *cobra.Command, args []string) {
-	if config.GetViperConfig() != nil {
+	if config.GetViperConfig(".") != nil {
 		return
 	}
 
@@ -44,10 +49,40 @@ func runBuild(cmd *cobra.Command, args []string) {
 
 	}
 
+	// Install dependencies
+	pythonPath, err := utils.FindVEnvExecutable(".venv", "python")
+	if err != nil {
+		pterm.Error.Println("Error finding python executable")
+		return
+	}
+
+	var pyinstaller *exec.Cmd
+
+	if buildOneFile {
+		pyinstaller = exec.Command(pythonPath, "-m", "pyinstaller", "--onefile", "main.py")
+	} else {
+		pyinstaller = exec.Command(pythonPath, "-m", "pyinstaller", "main.py")
+	}
+
+	err = pyinstaller.Run()
+	if err != nil {
+		pterm.Error.Println("Error building project")
+		return
+	}
+
+	if buildCompress {
+
+	}
+
+	pterm.Success.Println("Project built successfully!")
 }
 
 func init() {
 	buildCmd.Flags().StringVarP(&buildDestination, "out-dir", "o", "", "Destination directory where the project will be built")
 	buildCmd.Flags().StringVarP(&buildCustomName, "name", "n", "", "Custom name for the executable")
+	buildCmd.Flags().BoolVarP(&buildOneFile, "one-file", "f", false, "Build the project in one file")
+	buildCmd.Flags().BoolVarP(&buildCompress, "compress", "c", false, "Compress the output file(s) into a tarball file")
+	buildCmd.Flags().BoolVarP(&buildIncludeModels, "include-models", "m", false, "Include models in the build compressed file")
+
 	rootCmd.AddCommand(buildCmd)
 }
