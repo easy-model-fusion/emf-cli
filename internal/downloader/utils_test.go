7dfdf4b93b9a95f42487c5123ded1444412dd573
Zapharaos <matthieu.freitag@gmail.com>
2024-02-22 15:46:24 +0100

    put back removed tests from downloader utils

--- internal/downloader/utils_test.go
+++ internal/downloader/utils_test.go
@@ -2,69 +2,154 @@ package downloader
 
 import (
 	"github.com/easy-model-fusion/emf-cli/test"
+	"github.com/spf13/cobra"
 	"testing"
 )
 
-// TestDownloaderExecute_ArgsInvalid tests the Execute function with bad input.
-func TestDownloaderExecute_ArgsInvalid(t *testing.T) {
+// TestEmptyModel_True tests the EmptyModel to return true.
+func TestEmptyModel_True(t *testing.T) {
 	// Init
-	args := Args{}
+	sm := Model{}
 
 	// Execute
-	result, err := Execute(args)
+	result := EmptyModel(sm)
 
 	// Assert
-	test.AssertNotEqual(t, err, nil)
-	test.AssertEqual(t, result.IsEmpty, false)
+	test.AssertEqual(t, true, result)
 }
 
-// TestDownloaderExecute_ScriptError tests the Execute function with failing script.
-func TestDownloaderExecute_ScriptError(t *testing.T) {
-	// TODO : mock utils.ExecuteScript to return ([]byte{}, errors.New(""), 0)
+// TestEmptyModel_False tests the EmptyModel to return true.
+func TestEmptyModel_False(t *testing.T) {
+	// Init
+	sm := Model{
+		Path:   "/path/to/model",
+		Module: "module_name",
+		Class:  "class_name",
+	}
 
+	// Execute
+	result := EmptyModel(sm)
+
+	// Assert
+	test.AssertEqual(t, false, result)
+}
+
+// TestEmptyTokenizer_True tests the EmptyTokenizer to return true.
+func TestEmptyTokenizer_True(t *testing.T) {
+	// Init
+	st := Tokenizer{}
+
+	// Execute
+	result := EmptyTokenizer(st)
+
+	// Assert
+	test.AssertEqual(t, true, result)
+}
+
+// TestEmptyTokenizer_False tests the EmptyTokenizer to return true.
+func TestEmptyTokenizer_False(t *testing.T) {
+	// Init
+	st := Tokenizer{
+		Path:  "/path/to/tokenizer",
+		Class: "tokenizer_class",
+	}
+
+	// Execute
+	result := EmptyTokenizer(st)
+
+	// Assert
+	test.AssertEqual(t, false, result)
+}
+
+// TestArgsValidate_MissingName tests the ArgsValidate function to return an error.
+func TestArgsValidate_MissingName(t *testing.T) {
+	// Init
+	args := Args{ModelModule: "present"}
+
+	// Execute
+	result := ArgsValidate(args)
+
+	// Assert
+	test.AssertNotEqual(t, result, nil)
+}
+
+// TestArgsValidate_MissingModule tests the ArgsValidate function to return an error.
+func TestArgsValidate_MissingModule(t *testing.T) {
+	// Init
+	args := Args{ModelName: "present"}
+
+	// Execute
+	result := ArgsValidate(args)
+
+	// Assert
+	test.AssertNotEqual(t, result, nil)
+}
+
+// TestArgsValidate_Success tests the ArgsValidate function to succeed.
+func TestArgsValidate_Success(t *testing.T) {
 	// Init
 	args := Args{ModelName: "present", ModelModule: "present"}
 
 	// Execute
-	result, err := Execute(args)
+	result := ArgsValidate(args)
 
 	// Assert
-	test.AssertNotEqual(t, err, nil)
-	test.AssertEqual(t, result.IsEmpty, false)
+	test.AssertEqual(t, result, nil)
 }
 
-// TestDownloaderExecute_ResponseEmpty tests the Execute function with script returning no data.
-func TestDownloaderExecute_ResponseEmpty(t *testing.T) {
-	// TODO : mock utils.ExecuteScript to return (nil, nil, 0)
-	t.Skip()
-
+// TestArgsGetForCobra tests the ArgsGetForCobra.
+func TestArgsGetForCobra(t *testing.T) {
 	// Init
-	args := Args{ModelName: "present", ModelModule: "present"}
+	cmd := &cobra.Command{}
+	args := &Args{}
 
 	// Execute
-	result, err := Execute(args)
+	ArgsGetForCobra(cmd, args)
 
 	// Assert
-	test.AssertEqual(t, err, nil)
-	test.AssertEqual(t, result.IsEmpty, true)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(ModelName), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(ModelModule), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(ModelClass), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(ModelOptions), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(TokenizerClass), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(TokenizerOptions), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(Overwrite), nil)
+	test.AssertNotEqual(t, cmd.Flags().Lookup(Skip), nil)
+
+	test.AssertEqual(t, args.ModelName, "")
+	test.AssertEqual(t, args.ModelModule, "")
+	test.AssertEqual(t, args.ModelClass, "")
+	test.AssertEqual(t, len(args.ModelOptions), 0)
+	test.AssertEqual(t, args.TokenizerClass, "")
+	test.AssertEqual(t, len(args.TokenizerOptions), 0)
+	test.AssertEqual(t, args.Skip, "")
 }
 
-// TestDownloaderExecute_Success tests the Execute function with succeeding script.
-func TestDownloaderExecute_Success(t *testing.T) {
-
-	/*responseBadString := "{ \"bad\": \"property\" }"
-	bytes := []byte(responseBadString)*/
-
-	// TODO : mock utils.ExecuteScript to return (bytes, nil, 0)
-	t.Skip()
-
+// TestArgsProcessForPython tests the ArgsProcessForPython.
+func TestArgsProcessForPython(t *testing.T) {
 	// Init
-	args := Args{ModelName: "present", ModelModule: "present"}
+	args := Args{
+		ModelName:        "model",
+		ModelModule:      "module",
+		ModelClass:       "class",
+		ModelOptions:     []string{"opt1=val1", "opt2=val2"},
+		TokenizerClass:   "tokenizer",
+		TokenizerOptions: []string{"tok_opt1=val1"},
+		Skip:             "model",
+	}
+	expected := []string{
+		TagPrefix + EmfClient, TagPrefix + Overwrite,
+		"/path/to/download", "model", "module",
+		TagPrefix + ModelClass, "class",
+		TagPrefix + ModelOptions, "opt1=val1", "opt2=val2",
+		TagPrefix + TokenizerClass, "tokenizer",
+		TagPrefix + TokenizerOptions, "tok_opt1=val1",
+		TagPrefix + Skip, "model",
+	}
 
 	// Execute
-	result, err := Execute(args)
+	result := ArgsProcessForPython(args)
 
 	// Assert
-	test.AssertEqual(t, err, nil)
-	test.AssertEqual(t, result.IsEmpty, false)
+	test.AssertEqual(t, len(result), len(expected))
 }
