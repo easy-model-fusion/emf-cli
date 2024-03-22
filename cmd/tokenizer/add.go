e95523d54e3a9b2040f11efadd7fa4cfa8949796
omar <omarchamaalak@gmail.com>
2024-03-22 17:18:44 +0100

    Created files

--- cmd/tokenizer/add.go
+++ cmd/tokenizer/add.go
@@ -1,6 +1,7 @@
 package cmdtokenizer
 
 import (
+	"github.com/easy-model-fusion/emf-cli/internal/sdk"
 	"github.com/spf13/cobra"
 )
 
@@ -9,10 +10,12 @@ var tokenizerAddCmd = &cobra.Command{
 	Use:   "add <model name> <tokenizer name> [<other tokenizer names>...]",
 	Short: "Add one or more tokenizers",
 	Long:  "Add one or more tokenizers",
+	Args:  cobra.MinimumNArgs(1),
 	Run:   runTokenizerAdd,
 }
 
 // runTokenizerAdd runs the tokenizer add command
 func runTokenizerAdd(cmd *cobra.Command, args []string) {
+	sdk.SendUpdateSuggestion()
 
 }
