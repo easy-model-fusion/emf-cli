aefcfb97cac36b5b9b628024ad7b9c2105bc2ff8
Quentin <63395737+SkelNeXus@users.noreply.github.com>
2024-03-22 22:55:03 +0100

    Modifications on the generation, now it works with diffusers and tran… (#124)

--- internal/model/generation_test.go
+++ internal/model/generation_test.go
@@ -13,7 +13,7 @@ func TestModel_GenClass(t *testing.T) {
 		Path:        "build/stabilityai/sdxl-turbo",
 		PipelineTag: huggingface.TextToImage,
 		Module:      huggingface.DIFFUSERS,
-		Class:       huggingface.AutoDiffusers,
+		Class:       "DiffusionPipeline",
 	}
 
 	class := model.GenClass()
@@ -31,13 +31,13 @@ func TestModel_GenClass(t *testing.T) {
 	}
 }
 
-func TestModel_GenFile(t *testing.T) {
+func TestModel_DiffuserGenFile(t *testing.T) {
 	model := Model{
 		Name:        "stabilityai/sdxl-turbo",
 		Path:        "build/stabilityai/sdxl-turbo",
 		PipelineTag: huggingface.TextToImage,
 		Module:      huggingface.DIFFUSERS,
-		Class:       huggingface.AutoDiffusers,
+		Class:       "DiffusionPipeline",
 	}
 
 	file := model.GenFile()
@@ -56,6 +56,37 @@ func TestModel_GenFile(t *testing.T) {
 	test.AssertEqual(t, len(file.HeaderComments), 2, "The file should contain two header comments.")
 }
 
+func TestModel_TransformersGenFile(t *testing.T) {
+	model := Model{
+		Name:        "microsoft/phi-2",
+		Path:        "build/microsoft/phi-2/model",
+		PipelineTag: huggingface.TextGeneration,
+		Module:      huggingface.TRANSFORMERS,
+		Class:       "AutoModelForCausalLM",
+		Tokenizers: Tokenizers{
+			{
+				Class: "AutoTokenizer",
+				Path:  "build/microsoft/phi-2/AutoTokenizer",
+			},
+		},
+	}
+
+	file := model.GenFile()
+	gen := codegen.NewPythonCodeGenerator(true)
+	result, err := gen.Generate(file)
+
+	t.Logf("\n%s", result)
+
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	test.AssertEqual(t, file.Name, "MicrosoftPhi2.py", "The file name should be formatted correctly.")
+	test.AssertEqual(t, len(file.Classes), 1, "The file should contain one class.")
+	test.AssertEqual(t, len(file.HeaderComments), 2, "The file should contain two header comments.")
+}
+
 func TestModel_GetFormattedModelName(t *testing.T) {
 	model := Model{
 		Name:        "stabilityai/sdxl-turbo",
@@ -67,17 +98,122 @@ func TestModel_GetFormattedModelName(t *testing.T) {
 	test.AssertEqual(t, model.GetFormattedModelName(), "StabilityaiSdxlTurbo", "The model name should be formatted correctly.")
 }
 
-func TestModel_GetPipelineTagAbstractClassName(t *testing.T) {
+func TestModel_GetFormattedModelName_SpecialCharacters(t *testing.T) {
 	model := Model{
+		Name:        "2stabilityai/sdxl-turbo-2",
+		Path:        "build/stabilityai/sdxl-turbo-2",
 		PipelineTag: huggingface.TextToImage,
+		Module:      huggingface.DIFFUSERS,
 	}
 
-	test.AssertEqual(t, model.GetPipelineTagAbstractClassName(), "ModelTextToImage", "The model name should be formatted correctly.")
+	test.AssertEqual(t, model.GetFormattedModelName(), "Model2StabilityaiSdxlTurbo2", "The model name should be formatted correctly.")
+}
 
-	model.PipelineTag = huggingface.TextGeneration
+func TestModel_GetPipelineTagAbstractClassName(t *testing.T) {
+	model := Model{
+		Module: huggingface.DIFFUSERS,
+	}
 
-	test.AssertEqual(t, model.GetPipelineTagAbstractClassName(), "ModelTextToText", "The model name should be formatted correctly.")
+	test.AssertEqual(t, model.GetSDKClassNameWithModule(), "ModelDiffusers", "The model name should be formatted correctly.")
 
-	model.PipelineTag = "unknown"
-	test.AssertEqual(t, model.GetPipelineTagAbstractClassName(), "", "The model name should be formatted correctly.")
+	model.Module = huggingface.TRANSFORMERS
+
+	test.AssertEqual(t, model.GetSDKClassNameWithModule(), "ModelTransformers", "The model name should be formatted correctly.")
+
+	model.Module = "unknown"
+	test.AssertEqual(t, model.GetSDKClassNameWithModule(), "", "The model name should be formatted correctly.")
+}
+
+func TestModel_GetHuggingFaceClassImport(t *testing.T) {
+	model := Model{
+		Module: huggingface.DIFFUSERS,
+		Class:  "DiffusionPipeline",
+	}
+
+	test.AssertEqual(t, model.GetHuggingFaceClassImport(), "DiffusionPipeline", "The model name should be formatted correctly.")
+
+	model.Module = huggingface.TRANSFORMERS
+	model.Tokenizers = Tokenizers{
+		{
+			Class: "AutoTokenizer",
+		},
+	}
+
+	test.AssertEqual(t, model.GetHuggingFaceClassImport(), "DiffusionPipeline, AutoTokenizer", "The model name should be formatted correctly.")
+
+	model.Module = "unknown"
+	test.AssertEqual(t, model.GetHuggingFaceClassImport(), "", "The model name should be formatted correctly.")
+}
+
+func TestModel_GetModuleAutoPipelineClassName(t *testing.T) {
+	model := Model{
+		Module: huggingface.DIFFUSERS,
+	}
+
+	test.AssertEqual(t, model.GetModuleAutoPipelineClassName(), "DiffusionPipeline", "The model name should be formatted correctly.")
+
+	model.Module = huggingface.TRANSFORMERS
+
+	test.AssertEqual(t, model.GetModuleAutoPipelineClassName(), "AutoModel", "The model name should be formatted correctly.")
+
+	model.Module = "unknown"
+	test.AssertEqual(t, model.GetModuleAutoPipelineClassName(), "", "The model name should be formatted correctly.")
+}
+
+func TestModel_GenInitParamsWithModule(t *testing.T) {
+	model := Model{
+		Name:        "stabilityai/sdxl-turbo",
+		Path:        "build/stabilityai/sdxl-turbo",
+		PipelineTag: huggingface.TextToImage,
+		Module:      huggingface.DIFFUSERS,
+		Class:       "DiffusionPipeline",
+	}
+
+	// testing diffusers
+	params := model.GenInitParamsWithModule()
+	test.AssertEqual(t, len(params), 2, "The number of parameters should be correct.")
+
+	// testing transformers
+	model.Module = huggingface.TRANSFORMERS
+	params = model.GenInitParamsWithModule()
+	test.AssertEqual(t, len(params), 1, "The number of parameters should be correct.")
+
+	// testing unknown
+	model.Module = "unknown"
+	params = model.GenInitParamsWithModule()
+	test.AssertEqual(t, len(params), 0, "The number of parameters should be correct.")
+}
+
+func TestModel_GenSuperInitParamsWithModule(t *testing.T) {
+	model := Model{
+		Name:        "stabilityai/sdxl-turbo",
+		Path:        "build/stabilityai/sdxl-turbo",
+		PipelineTag: huggingface.TextToImage,
+		Module:      huggingface.DIFFUSERS,
+		Class:       "DiffusionPipeline",
+	}
+
+	// testing diffusers
+	params := model.GenSuperInitParamsWithModule()
+	test.AssertEqual(t, len(params), 4, "The number of parameters should be correct.")
+
+	// testing transformers without tokenizers
+	model.Module = huggingface.TRANSFORMERS
+	params = model.GenSuperInitParamsWithModule()
+	test.AssertEqual(t, len(params), 5, "The number of parameters should be correct.")
+
+	// testing transformers with tokenizers
+	model.Tokenizers = Tokenizers{
+		{
+			Class: "AutoTokenizer",
+			Path:  "build/stabilityai/sdxl-turbo/AutoTokenizer",
+		},
+	}
+	params = model.GenSuperInitParamsWithModule()
+	test.AssertEqual(t, len(params), 7, "The number of parameters should be correct.")
+
+	// testing unknown
+	model.Module = "unknown"
+	params = model.GenSuperInitParamsWithModule()
+	test.AssertEqual(t, len(params), 0, "The number of parameters should be correct.")
 }
