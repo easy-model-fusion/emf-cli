c146f35c18af5069b1b4c3c42d9fd4db5234f15f
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-03-23 16:16:55 +0100

    removed useless code

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,7 +4,6 @@ import (
 	"github.com/easy-model-fusion/emf-cli/internal/app"
 	"github.com/easy-model-fusion/emf-cli/internal/utils/stringutil"
 	"github.com/easy-model-fusion/emf-cli/pkg/huggingface"
-	"github.com/pterm/pterm"
 	"path"
 )
 
@@ -176,7 +175,6 @@ func (m *Model) UpdatePaths() {
 			m.Tokenizers[i].Path = path.Join(basePath, tokenizer.Class)
 		}
 	}
-	pterm.Info.Println(modelPath)
 	m.Path = modelPath
 }
 
