97935421cda739bed62b2f691762eb0cd6d0d2f3
Omar El Chamaa <62254601+OmarElChamaa@users.noreply.github.com>
2024-03-24 17:14:08 +0100

    #81 Model tokenizer update (#116)
    * Adding changes
    
    * Refactor
    
    * Moved files, and increased Coverage
    
    * Added Uts
    
    * lint fix
    
    * Update internal/model/model.go
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    
    * Update internal/model/model.go
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    
    * Update internal/controller/tokenizer/update_tokenizer.go
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    
    * Added comments
    
    * Changed test names to respect format
    
    * Removed duplicate test
    
    * Added comment
    
    * Update internal/controller/tokenizer/update_tokenizer.go
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    
    * Update internal/controller/tokenizer/update_tokenizer.go
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    
    * Fixes per PR
    
    * Fixes Per pr
    
    * Fixes Per pr
    
    * Fixes Per pr
    
    * Fixes Per pr
    
    * Final fix Per pr
    
    * Merged main and fixe errors
    
    * Small fix
    
    * Renaming files
    
    * missing Git init added
    
    * Wrapping controllers update and remove tokenizer in structs
    
    * Fix
    
    * Added error handling
    
    * Error fixes
    
    * Improved Uts and fixed typos
    
    * Logic fix
    
    * Update internal/controller/tokenizer/remove.go
    
    Co-authored-by: Paul <SchawnnDev@users.noreply.github.com>
    
    * Improved tests per PR
    
    ---------
    
    Co-authored-by: Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
    Co-authored-by: Paul <SchawnnDev@users.noreply.github.com>

---
+++ internal/controller/tokenizer/update_test.go
@@ -0,0 +1,262 @@
+package tokenizer
+
+import (
+	"github.com/easy-model-fusion/emf-cli/internal/app"
+	"github.com/easy-model-fusion/emf-cli/internal/config"
+	"github.com/easy-model-fusion/emf-cli/internal/downloader/model"
+	"github.com/easy-model-fusion/emf-cli/internal/model"
+	"github.com/easy-model-fusion/emf-cli/pkg/huggingface"
+	"github.com/easy-model-fusion/emf-cli/test"
+	"github.com/easy-model-fusion/emf-cli/test/mock"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	app.Init("", "")
+	app.InitGit("https://github.com/SchawnnDev", "")
+	os.Exit(m.Run())
+}
+
+// TestTokenizerUpdateCmd_ValidArgs tests the update command with valid args
+func TestTokenizerUpdateCmd_ValidArgs(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+
+	// Initialize selected models list
+	var args []string
+	args = append(args, "model1")
+	args = append(args, "tokenizer1")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+	ic := UpdateTokenizerController{}
+	// Process update
+	err = ic.TokenizerUpdateCmd(args)
+	expectedErrMsg := "the following tokenizer(s) couldn't be downloaded : [tokenizer1]"
+	test.AssertEqual(t, err.Error(), expectedErrMsg, "Unexpected error message")
+
+	_, err = config.GetModels()
+	test.AssertEqual(t, err, nil, "No error expected on getting models")
+}
+
+// TestTokenizerUpdateCmd_NoModuleTransformersUpdate tests the update command
+// with non-existing tokenizer path
+func TestTokenizerUpdateCmd_NoModuleTransformersUpdate(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Source: "CUSTOM",
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+	// Initialize selected models list
+	var args []string
+	args = append(args, "model1")
+	args = append(args, "tokenizer2")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+	ic := UpdateTokenizerController{}
+	// Process update
+
+	err = ic.TokenizerUpdateCmd(args)
+	expectedErrMsg := "only transformers models have tokenizers"
+	test.AssertEqual(t, err.Error(), expectedErrMsg, "Unexpected error message")
+
+	_, err = config.GetModels()
+	test.AssertEqual(t, err, nil, "No error expected on getting models")
+}
+
+// TestTokenizerUpdateCmd_WrongModelNameUpdate tests the update command
+// with the wrong model name
+func TestTokenizerUpdateCmd_WrongModelNameUpdate(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+	// Initialize selected models list
+	var args []string
+	args = append(args, "modelX")
+	args = append(args, "tokenizer1")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+
+	ic := UpdateTokenizerController{}
+	// Process update
+	err = ic.TokenizerUpdateCmd(args)
+	test.AssertEqual(t, err, nil, "Operation failed, no model found")
+}
+
+// TestTokenizerUpdateCmd_NoArgs tests the update command
+// with no args
+func TestTokenizerUpdateCmd_NoArgs(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+	// Initialize selected models list
+	var args []string
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+	ic := UpdateTokenizerController{}
+	// Process update
+	err = ic.TokenizerUpdateCmd(args)
+	expectedErrMsg := "enter a model in argument"
+	test.AssertEqual(t, err.Error(), expectedErrMsg, "Unexpected error message")
+
+}
+
+// TestTokenizerUpdateCmd_NoTokenizerInArgs tests the update command
+// with no tokenizers in args
+func TestTokenizerUpdateCmd_NoTokenizerInArgs(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+
+	var expectedSelections []string
+	expectedSelections = append(expectedSelections, "tokenizer1")
+
+	// Create ui mock
+	ui := mock.MockUI{MultiselectResult: expectedSelections}
+	app.SetUI(ui)
+
+	// Initialize selected models list
+	var args []string
+	args = append(args, "model1")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+
+	ic := UpdateTokenizerController{}
+	// Process update
+	err = ic.TokenizerUpdateCmd(args)
+	expectedErrMsg := "the following tokenizer(s) couldn't be downloaded : [tokenizer1]"
+	test.AssertEqual(t, err.Error(), expectedErrMsg, "Unexpected error message")
+
+}
+
+// TestTokenizerUpdateCmd_NoTokenizerInArgsDownload tests the update command
+// with no tokenizers in args and downloading
+func TestTokenizerUpdateCmd_NoTokenizerInArgsDownload(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+
+	var expectedSelections []string
+	expectedSelections = append(expectedSelections, "tokenizer1")
+
+	// Create ui mock
+	ui := mock.MockUI{MultiselectResult: expectedSelections}
+	app.SetUI(ui)
+
+	// Create Downloader mock
+	downloader := mock.MockDownloader{DownloaderModel: downloadermodel.Model{Path: "test"}, DownloaderError: nil}
+	app.SetDownloader(&downloader)
+
+	// Initialize selected models list
+	var args []string
+	args = append(args, "model1")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+
+	ic := UpdateTokenizerController{}
+	// Process update
+	err = ic.TokenizerUpdateCmd(args)
+	test.AssertEqual(t, err, nil, "No error expected while processing remove")
+}
+
+// TestTokenizerUpdateCmd_UpdateError tests the error return of the update command
+func TestTokenizerUpdateCmd_UpdateError(t *testing.T) {
+	var models model.Models
+	models = append(models, model.Model{
+		Name:   "model1",
+		Module: huggingface.TRANSFORMERS,
+		Tokenizers: model.Tokenizers{
+			{Path: "path1", Class: "tokenizer1", Options: map[string]string{"option1": "value1"}},
+		},
+	})
+
+	var expectedSelections []string
+	expectedSelections = append(expectedSelections, "tokenizer1")
+
+	// Create ui mock
+	ui := mock.MockUI{MultiselectResult: expectedSelections}
+	app.SetUI(ui)
+
+	// Create Downloader mock
+	downloader := mock.MockDownloader{
+		DownloaderModel: downloadermodel.Model{Path: "test"},
+		DownloaderError: os.ErrClosed,
+	}
+	app.SetDownloader(&downloader)
+
+	// Initialize selected models list
+	var args []string
+	args = append(args, "model1")
+
+	// Create temporary configuration file
+	ts := test.TestSuite{}
+	_ = ts.CreateFullTestSuite(t)
+	defer ts.CleanTestSuite(t)
+	err := setupConfigFile(models)
+	test.AssertEqual(t, err, nil, "No error expected while adding models to configuration file")
+	ic := UpdateTokenizerController{}
+	// Process update
+
+	err = ic.TokenizerUpdateCmd(args)
+	expectedErrMsg := "the following tokenizer(s) couldn't be downloaded : [tokenizer1]"
+	test.AssertEqual(t, err.Error(), expectedErrMsg, "Unexpected error message")
+}
--- internal/model/model_test.go
+++ internal/model/model_test.go
@@ -311,6 +311,20 @@ func TestUpdatePaths_TransformersTokenizers(t *testing.T) {
 	test.AssertEqual(t, model.Tokenizers[0].Path, path.Join(app.DownloadDirectoryPath, model.Name, "tokenizer"))
 }
 
+// TestFilterWithClass_Success tests the Tokenizers.FilterWithClass function to return the correct models.
+func TestFilterWithClass_Success(t *testing.T) {
+	//Init
+	tokenizers := GetTokenizers(2)
+	names := []string{tokenizers[0].Class, tokenizers[1].Class}
+
+	// Execute
+	result := tokenizers.FilterWithClass(names)
+
+	// Assert
+	test.AssertEqual(t, len(tokenizers), len(result), "Lengths should be equal.")
+
+}
+
 func TestTokenizers_Difference(t *testing.T) {
 	// Init
 	tokenizers := GetTokenizers(3)
