d8d214a89612fe7e98c1a84d94cfefc1d1307bd0
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-04-18 15:13:03 +0200

    remove redundant message

--- cmd/model/add.go
+++ cmd/model/add.go
@@ -37,7 +37,6 @@ func init() {
 func runAdd(cmd *cobra.Command, args []string) {
 	err := addController.Run(args, customArgs)
 	if err != nil {
-		app.UI().Error().Println(err.Error())
 		os.Exit(1)
 	}
 }
