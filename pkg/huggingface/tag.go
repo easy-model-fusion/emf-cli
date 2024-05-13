785fd5a6e7f505833e1f298d9408638f5a3fdec1
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-05-13 15:20:12 +0200

    added more compatible modules

--- pkg/huggingface/tag.go
+++ pkg/huggingface/tag.go
@@ -3,6 +3,11 @@ package huggingface
 var AllTags = []PipelineTag{
 	TextGeneration,
 	TextToImage,
+	TextToVideo,
+	TextToAudio,
+	TextTo3d,
+	ImageToText,
+	ImageToImage,
 }
 
 type PipelineTag string
@@ -10,6 +15,11 @@ type PipelineTag string
 const (
 	TextGeneration PipelineTag = "text-generation"
 	TextToImage    PipelineTag = "text-to-image"
+	TextToVideo    PipelineTag = "text-to-video"
+	TextTo3d       PipelineTag = "text-to-3d"
+	TextToAudio    PipelineTag = "text-to-audio"
+	ImageToText    PipelineTag = "image-to-text"
+	ImageToImage   PipelineTag = "image-to-image"
 )
 
 // AllTagsString returns all tags as a string slice
